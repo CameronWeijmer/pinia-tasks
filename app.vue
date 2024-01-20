c6678d13846523799fdc52df99f25b1a25352ed9
Cameron Weijmer <weijmer.c@gmail.com>
2024-01-21 00:39:44 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header>
+            <img src="./assets/pinia_logo.svg" alt="Pinia Logo">
+            <h1>Pinia Tasks</h1>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Your Nuxt App',
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+                },
+            ],
+        };
+    },
+
+};
+</script>
+
+<style>
+@import url("~/assets/main.css");
+</style>
