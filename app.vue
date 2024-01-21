6818cf53c689756f777c0de16f477c2f76a323c7
Cameron Weijmer <weijmer.c@gmail.com>
2024-01-21 21:56:26 +0100

    feat: resetting State & storeToRefs

--- app.vue
+++ app.vue
@@ -18,27 +18,33 @@
         </nav>
 
         <!-- loading -->
-        <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+        <div class="loading" v-if="loading">Loading tasks...</div>
 
         <!-- task list -->
         <div class="task-list" v-if="filter === 'all'">
-            <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-            <div v-for="task in taskStore.tasks">
+            <p>You have {{ totalCount }} tasks left to do</p>
+            <div v-for="task in tasks">
                 <TaskDetails :task="task" />
             </div>
         </div>
         <div class="task-list" v-if="filter === 'favs'">
-            <p>You have {{ taskStore.favCount }} favs left to do</p>
+            <p>You have {{ favCount }} favs left to do</p>
 
-            <div v-for="task in taskStore.favs">
+            <div v-for="task in favs">
                 <TaskDetails :task="task" />
             </div>
         </div>
+
+        <button @click="taskStore.$reset">Reset state</button>
+
     </div>
 </template>
 
 <script setup>
 const taskStore = useTaskStore()
+
+const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore)
+
 const filter = ref("all")
 
 // fetch tasks
